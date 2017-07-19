72839a0743104109b0ebc354112b6464a7e06df4
Pandas <daria-shcherbatyuk@yandex.ru>
2017-07-19 19:14:25 +0300

    optimized cocktail sort

--- Sorting algorithms/cocktail sort.py
+++ Sorting algorithms/cocktail sort.py
@@ -1,6 +1,10 @@
 import random
+import time
 
-def sort (x):
+N = 10000
+
+
+def std_cocktail_sort (x):
     left = 0
     right = len(x)-1
 
@@ -15,21 +19,76 @@ def sort (x):
                 x[i-1], x[i] = x[i], x[i-1]
         left = left + 1
 
-    return(x)
+    return x
 
 
-a = []
-b = []
-c = []
-for i in range(100):
-    a.append(i)
-    b.append(100-i)
-    c.append(random.randint(-100, 100))
+def opt_cocktail_sort(x):
+    for i in range(len(x)//2):
+        swap = False
+        for j in range(1+i, len(x)-i):
+            if x[j] < x[j-1]:
+                x[j], x[j - 1] = x[j - 1], x[j]
+                swap = True
+        if not swap:
+            break
+        swap = False
+        for j in range(len(x)-i-1, i, -1):
+            if x[j] < x[j-1]:
+                x[j], x[j - 1] = x[j - 1], x[j]
+                swap = True
+        if not swap:
+            break
 
-a = sort(a)
-b = sort(b)
-c = sort(c)
+    return x
 
-print(a)
-print(b)
-print(c)
+
+if __name__ == "__main__":
+    a = []
+    b = []
+    c = []
+    for i in range(N):
+        a.append(i)
+        b.append(N-i)
+        c.append(random.randint(-N, N))
+
+    print("NO OPTIMIZED std_bubble_sort(x)")
+
+    print("\n...SORTING ARRAY...")
+    st = time.time()
+    na = std_cocktail_sort(a)
+    end = time.time()
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n...REVERSE ARRAY...")
+    st = time.time()
+    nb = std_cocktail_sort(b)
+    end = time.time()
+    print( "\nTIME:", (end - st), "  sec")
+
+    print("\n...RANDOM ARRAY...")
+    st = time.time()
+    nc = std_cocktail_sort(c)
+    end = time.time()
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n\nOPTIMIZED opt_bubble_sort(x)")
+
+    print("\n...SORTING ARRAY...")
+    st = time.time()
+    na = opt_cocktail_sort(a)
+    end = time.time()
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n...REVERSE ARRAY...")
+    st = time.time()
+    nb = opt_cocktail_sort(b)
+    end = time.time()
+    print("\nTIME:", (end - st), "  sec")
+
+    print("\n...RANDOM ARRAY...")
+    st = time.time()
+    nc = opt_cocktail_sort(c)
+    end = time.time()
+    print("\nTIME:", (end - st), "  sec")
+
+
