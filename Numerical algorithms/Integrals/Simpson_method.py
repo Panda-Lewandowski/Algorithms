7fdf5eac01c0ad36df7f01238eea5bf855001d52
Pandas <daria-shcherbatyuk@yandex.ru>
2018-04-09 16:57:43 +0300

    Update Simpson_method.py

--- Numerical algorithms/Integrals/Simpson_method.py
+++ Numerical algorithms/Integrals/Simpson_method.py
@@ -1,11 +1,15 @@
-def f(x):
-    return x**x
+def simpson(func, *args, right, left, n):
+    h = (right - left) / n
 
+    ans = h / 3
+    even = 0.0
+    odd = 0.0
 
-def simpson(a,b,n):
-    h = abs(b-a)/n
-    S = 0
-    while fabs(a-b)>0.0001:
-        S += f(a)+f(a+h)+4*f(a+h/2)
-        a += h
-    return S*h/6
+    for i in range(1, n):
+        if i % 2 == 0:
+            even += func(left + h * i, *args)
+        else:
+            odd += func(left + h * i, *args)
+
+    ans *= (2 * even + 4 * odd + func(left, *args) + func(right, *args))
+    return ans
